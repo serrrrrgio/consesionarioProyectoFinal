87a3a9e59a8567fe2f018c6ae54808c12f18d417
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2024-10-26 08:57:24 -0500

    Se agrega la clase carro, y todos los enum correspondientes a este.

---
+++ src/main/java/co/edu/uniquindio/poo/model/Combustible.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.poo.model;
+
+public enum Combustible {
+    HIBRIDO, DIESEL, GASOLINA, ELECTRICO,
+}
---
+++ src/main/java/co/edu/uniquindio/poo/model/Transmision.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.poo.model;
+
+public enum Transmision {
+    MANUAL, AUTOMATICA,
+}
